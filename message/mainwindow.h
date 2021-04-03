8494424d5577a7144f1af3084cda1e516dca6b40
tn1511 <40852860+tn1511@users.noreply.github.com>
2021-04-03 17:44:52 +0100

    Add files via upload

---
+++ message/mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMqttClient>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+    void setClientPort(int p);
+
+private slots:
+    void on_buttonConnect_clicked();
+    void on_buttonQuit_clicked();
+    void updateLogStateChange();
+
+    void brokerDisconnected();
+
+    void on_buttonPublish_clicked();
+
+    void on_buttonSubscribe_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QMqttClient *m_client;
+};
+
+#endif // MAINWINDOW_H
